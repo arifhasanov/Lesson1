0d9f6885ee938340c665a786e36002dd86553dbf
arifhasanov@yahoo.com <37578820+arifhasanov@users.noreply.github.com>
2021-10-26 21:31:14 +0200

    my first commit

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+            String today = DateTime.Today.ToShortDateString();
+            Console.WriteLine($"Hello {name}!, today is {today}");
+            Console.Read();
+        }
+    }
+}
